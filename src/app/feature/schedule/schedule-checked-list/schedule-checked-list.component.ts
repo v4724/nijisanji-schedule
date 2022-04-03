0669e0a7105d323c99374b0d8f9c6ec7ebb644ac
v4724 <v47244724@gmail.com>
2022-04-04 06:18:58 +0800

    update schedule

--- src/app/feature/schedule/schedule-checked-list/schedule-checked-list.component.ts
+++ src/app/feature/schedule/schedule-checked-list/schedule-checked-list.component.ts
@@ -27,8 +27,8 @@ export class ScheduleCheckedListComponent implements OnInit {
   date: Moment = moment()
   displayWeekText: string = ''
 
-  newScheduleId: number = 621
-  newScheduleDay: number = 27
+  newScheduleId: number = 748
+  newScheduleDay: number = 3
 
   updateInfo: Map<Streamer, boolean> = new Map<Streamer, boolean>()
 
--- src/app/feature/schedule/toolbar/toolbar.component.ts
+++ src/app/feature/schedule/toolbar/toolbar.component.ts
@@ -11,7 +11,7 @@ export class ToolbarComponent implements OnInit {
   scheduleUpdatedTime: string = ''
 
   constructor() {
-    this.scheduleUpdatedTime = moment(1648944708336).format('YYYY-MM-DD HH:mm')
+    this.scheduleUpdatedTime = moment(1649024182696).format('YYYY-MM-DD HH:mm')
   }
 
   ngOnInit(): void {
