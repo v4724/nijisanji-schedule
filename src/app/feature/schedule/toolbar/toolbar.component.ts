65137f5568bb8098c70b44cc14c428ff6261fd1e
v4724 <v47244724@gmail.com>
2022-04-09 15:13:06 +0800

    update schedule

--- src/app/feature/schedule/toolbar/toolbar.component.ts
+++ src/app/feature/schedule/toolbar/toolbar.component.ts
@@ -11,7 +11,7 @@ export class ToolbarComponent implements OnInit {
   scheduleUpdatedTime: string = ''
 
   constructor() {
-    this.scheduleUpdatedTime = moment(1649378991236).format('YYYY-MM-DD HH:mm')
+    this.scheduleUpdatedTime = moment(1649488318162).format('YYYY-MM-DD HH:mm')
   }
 
   ngOnInit(): void {
