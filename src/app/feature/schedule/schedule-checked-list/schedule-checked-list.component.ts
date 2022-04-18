ab6f62b0996a6bde1a6ebedbc5591eef98c5a692
v4724 <v47244724@gmail.com>
2022-04-18 09:12:01 +0800

    update schedule

--- src/app/feature/schedule/schedule-checked-list/schedule-checked-list.component.ts
+++ src/app/feature/schedule/schedule-checked-list/schedule-checked-list.component.ts
@@ -27,8 +27,8 @@ export class ScheduleCheckedListComponent implements OnInit {
   date: Moment = moment()
   displayWeekText: string = ''
 
-  newScheduleId: number = 882
-  newScheduleDay: number = 10
+  newScheduleId: number = 1012
+  newScheduleDay: number = 17
 
   updateInfo: Map<Streamer, boolean> = new Map<Streamer, boolean>()
 
